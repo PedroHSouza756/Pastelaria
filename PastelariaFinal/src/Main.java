10975063742805c6df54bb12ea94b2fdd5d58305
Pedro H. S. e Souza <166566822+PedroHSouza756@users.noreply.github.com>
2025-10-29 15:00:39 -0300

    Delete PastelariaFinal directory

--- PastelariaFinal/src/Main.java
+++
@@ -1,7 +0,0 @@
-import view.TelaPrincipal;
-
-public class Main {
-    public static void main(String[] args) {
-        TelaPrincipal telaprincipal = new TelaPrincipal();
-    }
-}
--- PastelariaFinal/src/dao/DAOProduto.java
+++
@@ -1,8 +0,0 @@
-package dao;
-import java.sql.SQLException;
-import java.util.List;
-import model.Produto;
-public interface DAOProduto {
-    // todos os metodos de ProdutoSQL de uma forma mais simples de compreender (Interface)
-    public List<Produto> listarTipos() throws SQLException;
-}
